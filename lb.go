63090a0d5d116b2c598a55366d19b7abfb2d5f14
tb1995 <tb1995@gmail.com>
2024-03-30 15:50:33 +0500

    servers config

--- lb.go
+++ lb.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprint(w, "Hello world")
+	resp, err := http.Get("http://server4:8080")
+	if err != nil {
+		fmt.Fprint(w, "Something went wrong")
+	}
+	body, err := io.ReadAll(resp.Body)
+	sb := string(body)
+	fmt.Fprint(w, sb)
 }
 
 func main() {
